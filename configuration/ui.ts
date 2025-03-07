1b10cdd288962546fac99de6fde7215d6df9613a
JonathanTZhao <112503936+JonathanTZhao@users.noreply.github.com>
2025-03-07 16:15:08 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,7 +1,7 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Jonathan Zhao's AI`;
-export const MESSAGE_PLACEHOLDER: string = `What would you like to ask?`;
+export const CHAT_HEADER: string = `Jonathan Zhao's AI Assistant Jarvis`;
+export const MESSAGE_PLACEHOLDER: string = `What would you like to ask about Jonathan??`;
 export const FOOTER_MESSAGE: string = `Jonathan Zhao 2025`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Text`;
 export const PAGE_TITLE: string = `Jonathan Zhao's AI`;
